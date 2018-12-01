ffa5277481dd66c5377ba14aefa6daf3cdd5208d
Alex Devero <deveroalex@gmail.com>
2018-12-01 16:27:34 +0100

    Update cards on activity page

--- src/pages/activity.tsx
+++ src/pages/activity.tsx
@@ -1,14 +1,19 @@
 // Imports
 import * as React from 'react'
 
+import { CardActivityComments } from './../components/CardActivityComments'
+import { CardActivityMember } from './../components/cardActivityMember'
+import { CardActivityProject } from './../components/cardActivityProject'
+import { Dropdown } from './../components/dropdown'
+
 export class Activity extends React.Component {
     state = {
-        eventsMenuOpened: false
+        isProjectsMenuOpened: false
     }
 
-    handleEventsMenu = () => {
+    handleProjectsMenu = () => {
         this.setState({
-            eventsMenuOpened: !this.state.eventsMenuOpened
+            isProjectsMenuOpened: !this.state.isProjectsMenuOpened
         })
     }
 
@@ -26,11 +31,11 @@ export class Activity extends React.Component {
                         {/* Filter for mobile devices */}
                         <ul className="filter__nav filter__nav--mobile">
                             <li className="dropdown__container">
-                                <a className="dropdown__toggler" href="" onClick={this.handleEventsMenu}>
+                                <a className="dropdown__toggler" href="">
                                     All events <span>{String.fromCharCode(9662)}</span>
                                 </a>
 
-                                <ul className={'dropdown__list dropdown__list--centered' + (this.state.eventsMenuOpened ? ' dropdown__list--visible' : '')}>
+                                <ul className={'dropdown__list dropdown__list--centered' + (this.state.isProjectsMenuOpened ? ' dropdown__list--visible' : '')}>
                                     <li className="dropdown__item">
                                         <a className="dropdown__link" href="">Messages</a>
                                     </li>
@@ -67,12 +72,94 @@ export class Activity extends React.Component {
 
                         <ul className="filter__nav filter__nav--desktop filter__nav--right">
                             <li>
-                                <a href="#">Spacebook<span> {String.fromCharCode(9662)}</span></a>
+                                <Dropdown
+                                    align="right"
+                                    handleDropdownClick={this.handleProjectsMenu}
+                                    hasDividers={true}
+                                    isDropdownOpen={this.state.isProjectsMenuOpened}
+                                    items={['Spacebook', 'Playbook', 'Comicbook']}
+                                    label={['Showing ', <strong key="drp_str_0">Spacebook</strong>, <span key="drp_str_1">{String.fromCharCode(9662)}</span>]}
+                                />
                             </li>
                         </ul>
                     </div>
                 </div>
 
+                <div className="page__body body--activity">
+                    <div className="container">
+                        <div className="row activity__row">
+                            <CardActivityComments
+                                comments={[
+                                    [
+                                        ['Jiri Trecak'],
+                                        ['We really need this like yesterday. What’s the hold up?'],
+                                        [require('../images/avatar-jiri.png')]
+                                    ],
+                                    [
+                                        ['Maris Naylor'],
+                                        ['Sadly, I don’t think there is enough tea in the world to make this a pleasant task.'],
+                                        [require('../images/avatar-drax.png')]
+                                    ],
+                                    [
+                                        ['Vaclav Lustek'],
+                                        ['Mkay, let me make some tea and get to it.'],
+                                        [require('../images/avatar-iron-man.png')]
+                                    ]
+                                ]}
+                                numOfComments={3}
+                                nameOfProject="Supernova Example"
+                                nameOfScreen="Login"
+                                time="10 min ago"
+                            />
+
+                            <CardActivityMember
+                                avatarImageUrl={require('../images/avatar-jiri.png')}
+                                cardType="joined"
+                                teamName="S.T.A.R. Labs"
+                                teamSize="8"
+                                time="2:45 pm"
+                                username="Jiri Trecak"
+                            />
+
+                            <CardActivityMember
+                                avatarImageUrl={require('../images/avatar-drax.png')}
+                                cardType="left"
+                                teamName="S.T.A.R. Labs"
+                                teamSize="8"
+                                time="9:13 am"
+                                username="Eobard Thawne"
+                            />
+
+                            <CardActivityProject
+                                avatarImageUrl={require('../images/avatar-jiri.png')}
+                                cardType="updated"
+                                numOfScreens={8}
+                                projectName="Supernova Example"
+                                projectThumbnail={require('../images/spacebook-thumbnail-small.png')}
+                                projectThumbnailAlt="Supernova Example"
+                                screensAdded={2}
+                                screensRemoved={3}
+                                screensUpdated={8}
+                                time="1:30 pm"
+                                username="Jiri Trecak"
+                            />
+
+                            <CardActivityProject
+                                avatarImageUrl={require('../images/avatar-jiri.png')}
+                                cardType="created"
+                                numOfScreens={8}
+                                projectName="Supernova Example"
+                                projectThumbnail={require('../images/spacebook-thumbnail-small.png')}
+                                projectThumbnailAlt="Supernova Example"
+                                screensAdded={0}
+                                screensRemoved={0}
+                                screensUpdated={0}
+                                time="1:30 pm"
+                                username="Jiri Trecak"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

2699f6b995141ea15aa8aa67e033cf371b9f49a1
Alex Devero <deveroalex@gmail.com>
2018-12-02 10:53:58 +0100

    Add routes for personal pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,12 @@ import * as ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import { Activity } from './pages/activity'
+import { Account } from './pages/account'
 import { Home } from './pages/home'
+import { Plan } from './pages/plan'
 import { Projects } from './pages/projects'
 import { Organization } from './pages/organization'
+import { Settings } from './pages/settings'
 
 import { Header } from './components/header'
 
@@ -23,6 +26,9 @@ class App extends React.Component {
                     <Route path="/projects" component={Projects} />
                     <Route path="/activity" component={Activity} />
                     <Route path="/organization" component={Organization} />
+                    <Route path="/account" component={Account} />
+                    <Route path="/plan" component={Plan} />
+                    <Route path="/settings" component={Settings} />
                 </React.Fragment>
             </BrowserRouter>
         )

f4813496420c8b69d1676f50e41e7ee4cff3f2f5
Alex Devero <deveroalex@gmail.com>
2018-12-02 10:53:42 +0100

    Add template for account page

---
+++ src/pages/account.tsx
@@ -0,0 +1,19 @@
+// Imports
+import * as React from 'react'
+
+export class Account extends React.Component {
+    render() {
+        return(
+            <div className="page--account">
+                <div className="page__title">
+                    <div className="container">
+                        <h1 className="h2">Welcome back commander.</h1>
+                    </div>
+                </div>
+
+                <div className="page__body" />
+            </div>
+        )
+    }
+}
+

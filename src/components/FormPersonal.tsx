f1fd74d8c376e504c02649e2b52e6bfb490e7d3c
Alex Devero <deveroalex@gmail.com>
2018-12-05 15:24:47 +0100

    Add input handler

--- src/components/FormPersonal.tsx
+++ src/components/FormPersonal.tsx
@@ -13,6 +13,14 @@ export class FormPersonal extends React.Component<FormPersonalProps, {}> {
         inputBio: ''
     }
 
+    handleInputChange = (event) => {
+        event.preventDefault()
+
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     handleFormSubmit = (event) => {
         event.preventDefault()
     }
@@ -24,37 +32,37 @@ export class FormPersonal extends React.Component<FormPersonalProps, {}> {
                 <fieldset>
                     <label htmlFor="inputName">Name</label>
 
-                    <input id="inputName" name="inputName" type="text" defaultValue={this.state.inputName} />
+                    <input id="inputName" name="inputName" type="text" onChange={this.handleInputChange} defaultValue={this.state.inputName} />
                 </fieldset>
 
                 <fieldset>
                     <label htmlFor="inputNickname">Nickname</label>
 
-                    <input id="inputNickname" name="inputNickname" type="text" defaultValue={this.state.inputNickname} />
+                    <input id="inputNickname" name="inputNickname" type="text" onChange={this.handleInputChange} defaultValue={this.state.inputNickname} />
                 </fieldset>
 
                 <fieldset>
                     <label htmlFor="inputEmail">Email</label>
 
-                    <input id="inputEmail" name="inputEmail" type="email" defaultValue={this.state.inputEmail} />
+                    <input id="inputEmail" name="inputEmail" type="email" onChange={this.handleInputChange} defaultValue={this.state.inputEmail} />
                 </fieldset>
 
                 <fieldset>
                     <label htmlFor="inputPortfolioUrl">Portfolio URL</label>
 
-                    <input id="inputPortfolioUrl" name="inputPortfolioUrl" type="text" defaultValue={this.state.inputPortfolioUrl} />
+                    <input id="inputPortfolioUrl" name="inputPortfolioUrl" type="text" onChange={this.handleInputChange} defaultValue={this.state.inputPortfolioUrl} />
                 </fieldset>
 
                 <fieldset>
                     <label htmlFor="inputLocation">Location</label>
 
-                    <input id="inputLocation" name="inputLocation" type="text" defaultValue={this.state.inputLocation} />
+                    <input id="inputLocation" name="inputLocation" type="text" onChange={this.handleInputChange} defaultValue={this.state.inputLocation} />
                 </fieldset>
 
                 <fieldset>
                     <label htmlFor="inputBio">Bio</label>
 
-                    <textarea name="inputBio" id="inputBio" defaultValue={this.state.inputBio}></textarea>
+                    <textarea name="inputBio" id="inputBio" onChange={this.handleInputChange} defaultValue={this.state.inputBio}></textarea>
                 </fieldset>
 
                 <fieldset>

f62020329ecb5a3f0da4fb2f48160479fb9d0024
Alex Devero <deveroalex@gmail.com>
2018-12-11 16:09:04 +0100

    Add component for vertical plan

---
+++ src/components/pricing-plan-vertical.tsx
@@ -0,0 +1,176 @@
+// Imports
+import * as React from 'react'
+
+import { Button } from './button'
+
+type PricingPlanVerticalProps = {
+    isYearly: boolean;
+    type: 'Design' | 'Code' | 'Ultimate' | 'Free';
+}
+
+export class PricingPlanVertical extends React.PureComponent<PricingPlanVerticalProps, {}> {
+    generatePlan = () => {
+        switch (this.props.type) {
+            case 'Code':
+                return (
+                    <div className="plan plan--vertical">
+                        <div className="plan__head">
+                            <span className="plan__icon icon icon--code" />
+
+                            <h3 className="plan__title">Code</h3>
+
+                            <h4 className="plan__subtitle">Exports code so you can innovate.</h4>
+
+                            <span className="plan__price">{String.fromCharCode(36)}25 <span className="plan__period">{this.props.isYearly ? '/year' : '/month'}</span></span>
+
+
+
+                            <span className="plan__save-note">Save $78 a year by paying annually.</span>
+                        </div>
+
+                        <div className="plan__body">
+                            <ul className="plan__features">
+                                <li><span className="plan__feature-icon"></span> Full Prototyping Suite</li>
+
+                                <li><span className="plan__feature-icon"></span> Code Preview</li>
+
+                                <li><span className="plan__feature-icon"></span> Unlimited Teams</li>
+
+                                <li><span className="plan__feature-icon"></span> 1 Cloud Project</li>
+
+                                <li><span className="plan__feature-icon"></span> Export to iOS</li>
+
+                                <li><span className="plan__feature-icon"></span> Export to Android</li>
+
+                                <li><span className="plan__feature-icon"></span> Export to React Native</li>
+                            </ul>
+                        </div>
+
+                        <div className="plan__footer">
+                            <Button label={`Upgrade to ${this.props.type}`} type="primary" />
+                        </div>
+                    </div>
+                )
+
+            case 'Design':
+                return (
+                    <div className="plan plan--vertical">
+                        <div className="plan__head">
+                            <span className="plan__icon icon icon--design" />
+
+                            <h3 className="plan__title">Design</h3>
+
+                            <h4 className="plan__subtitle">Perfect for multiple projects.</h4>
+
+                            <span className="plan__price">{String.fromCharCode(36)}15 <span className="plan__period">{this.props.isYearly ? '/year' : '/month'}</span></span>
+
+
+
+                            <span className="plan__save-note">Save $78 a year by paying annually.</span>
+                        </div>
+
+                        <div className="plan__body">
+                            <ul className="plan__features">
+                                <li><span className="plan__feature-icon"></span> Full Prototyping Suite</li>
+
+                                <li><span className="plan__feature-icon"></span> Studio CodeX Preview</li>
+
+                                <li><span className="plan__feature-icon"></span> Cloud CodeX</li>
+
+                                <li><span className="plan__feature-icon"></span> Unlimited Teams</li>
+
+                                <li><span className="plan__feature-icon"></span> Unlimited Cloud Projects</li>
+                            </ul>
+                        </div>
+
+                        <div className="plan__footer">
+                            <Button label={`Upgrade to ${this.props.type}`} type="primary" />
+                        </div>
+                    </div>
+                )
+
+            case 'Ultimate':
+                return (
+                    <div className="plan plan--vertical">
+                        <div className="plan__head">
+                            <span className="plan__icon icon icon--ultimate" />
+
+                            <h3 className="plan__title">Ultimate</h3>
+
+                            <h4 className="plan__subtitle">End-to-end design to code.</h4>
+
+                            <span className="plan__price">{String.fromCharCode(36)}35 <span className="plan__period">{this.props.isYearly ? '/year' : '/month'}</span></span>
+
+                            <span className="plan__save-note">Save $78 a year by paying annually.</span>
+                        </div>
+
+                        <div className="plan__body">
+                            <ul className="plan__features">
+                                <li><span className="plan__feature-icon"></span> Full Prototyping Suite</li>
+
+                                <li><span className="plan__feature-icon"></span> Code Preview</li>
+
+                                <li><span className="plan__feature-icon"></span> Unlimited Teams</li>
+
+                                <li><span className="plan__feature-icon"></span> Unlimited Cloud Projects</li>
+
+                                <li><span className="plan__feature-icon"></span> Export to iOS</li>
+
+                                <li><span className="plan__feature-icon"></span> Export to Android</li>
+
+                                <li><span className="plan__feature-icon"></span> Export to React Native</li>
+
+                                <li><span className="plan__feature-icon"></span> Premium Support</li>
+                            </ul>
+                        </div>
+
+                        <div className="plan__footer">
+                            <Button label={`Upgrade to ${this.props.type}`} type="primary" />
+                        </div>
+                    </div>
+                )
+
+            default:
+                return (
+                    <div className="plan plan--vertical">
+                        <div className="plan__head">
+                            <span className="plan__icon icon icon--:" />
+
+                            <h3 className="plan__title">Free</h3>
+
+                            <h4 className="plan__subtitle">Get started here. Forever free!</h4>
+
+                            <span className="plan__price">{String.fromCharCode(36)}0 <span className="plan__period">{this.props.isYearly ? '/year' : '/month'}</span></span>
+
+                            <span className="plan__save-note">Save $78 a year by paying annually.</span>
+                        </div>
+
+                        <div className="plan__body">
+                            <ul className="plan__features">
+                                <li><span className="plan__feature-icon"></span> Full Prototyping Suite</li>
+
+                                <li><span className="plan__feature-icon"></span> CodeX Preview</li>
+
+                                <li><span className="plan__feature-icon"></span> Unlimited Teams</li>
+
+                                <li><span className="plan__feature-icon"></span> 1 Cloud Project</li>
+                            </ul>
+                        </div>
+
+                        <div className="plan__footer">
+                            <Button label={`Upgrade to ${this.props.type}`} type="primary" />
+                        </div>
+                    </div>
+                )
+        }
+    }
+
+    // Render the app
+    render() {
+        return (
+            <React.Fragment>
+                {this.generatePlan()}
+            </React.Fragment>
+        )
+    }
+}

aabab7815070885b049f42610ed00d5669820b8f
Alex Devero <deveroalex@gmail.com>
2018-12-11 12:23:25 +0100

    Update plans selection spacing

--- src/pages/plans/plan-selection.tsx
+++ src/pages/plans/plan-selection.tsx
@@ -15,11 +15,11 @@ export class PlanSelection extends React.PureComponent<PlanSelectionProps, {}> {
     render() {
         return (
             <div className="settings__content">
-                <h2 className="h3">Personal Plan</h2>
+                <h2 className="h3 mb-1">Personal Plan</h2>
 
                 <p className="form__note">You are currently on a <strong>Free plan</strong>.</p>
 
-                <p className="form__note">Upgrade to get access to all features that will save you a ton of time, such as CodeX, Cloud and more!</p>
+                <p className="form__note mb-2">Upgrade to get access to all features that will save you a ton of time, such as CodeX, Cloud and more!</p>
 
                 <PricingPlan isYearly={this.state.isYearly} isPopular={false} type="Design" isVertical={false} />
 
@@ -29,9 +29,9 @@ export class PlanSelection extends React.PureComponent<PlanSelectionProps, {}> {
 
                 <PricingPlan isYearly={this.state.isYearly} isPopular={false} type="Free" isVertical={false} />
 
-                <h3 className="h3">Teams &amp; Organizations</h3>
+                <h3 className="h3 mt-4">Teams &amp; Organizations</h3>
 
-                <p className="form__note">Looking for advanced team support and access rights, consolidated billing, better security and many more features? We offer team plans for both smaller and larger organizations. Create an organization to get started.</p>
+                <p className="form__note mt-1 mb-3">Looking for advanced team support and access rights, consolidated billing, better security and many more features? We offer team plans for both smaller and larger organizations. Create an organization to get started.</p>
 
                 <Button label="Create new organization" type="primary" />
             </div>

ec03b846f58e989be34a8c21c8ea7c4b485afb9e
Alex Devero <deveroalex@gmail.com>
2018-12-14 16:22:28 +0100

    Add alpha modal

--- src/pages/organization.tsx
+++ src/pages/organization.tsx
@@ -1,12 +1,22 @@
 // Imports
 import * as React from 'react'
+import Modal from 'react-modal'
 
 import { Button } from 'src/components/button'
 import { EmptyMessageOrg } from 'src/components/empty-message-org'
 
+Modal.setAppElement('#root')
+
 export class Organization extends React.Component {
     state = {
-        isMemberOfOrg: true
+        isMemberOfOrg: true,
+        isModalInviteOpen: false
+    }
+
+    handleModal = () => {
+        this.setState({
+            isModalInviteOpen: !this.state.isModalInviteOpen
+        })
     }
 
     handleRowClick = (event) => {
@@ -93,7 +103,7 @@ export class Organization extends React.Component {
                                                 <a className="link--blue" href="#">Pending Invitations (1)</a>
                                             </li>
 
-                                            <li>
+                                            <li onClick={this.handleModal}>
                                                 <Button label="Invite Members" type="primary" />
                                             </li>
                                         </ul>
@@ -112,6 +122,89 @@ export class Organization extends React.Component {
                             )
                         }
                     </div>
+
+                    <Modal
+                        contentLabel="Invite"
+                        isOpen={this.state.isModalInviteOpen}
+                        onRequestClose={this.handleModal}
+                        shouldCloseOnOverlayClick={true}
+                    >
+                        <div className="modal__header">Invite</div>
+
+                        <div className="modal__body">
+                            <div className="row align-items-center">
+                                <div className="col-md-6">Invite as</div>
+
+                                <div className="col-md-6">
+                                    <select id="tableOrgSelect" name="tableOrgSelect" defaultValue="Owner">
+                                        <option value="admin">Admin</option>
+                                        <option value="client">Client</option>
+                                        <option value="member">Member</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <p className="form__note mt-1 mb-2">Administrators can add and remove organization members, create projects, and have access to all features of Supernova Ultimate.</p>
+
+                            <div>
+                                <label htmlFor="inviteEmailAddresses">Email Addresses <span>Separated by commas</span></label>
+
+                                <textarea name="inviteEmailAddresses" id="inviteEmailAddresses" />
+                            </div>
+                        </div>
+
+                        <div className="modal__table table table--default">
+                            <div className="table__row">
+                                <div className="table__cell table__cell--left">
+                                    <strong className="table__cell-title">Organization seats</strong>
+
+                                    <p className="table__cell-text">0 seats available</p>
+                                </div>
+
+                                <div className="table__cell table__cell--right">
+                                    <strong>Add additional seats</strong>
+
+                                    <input type="number" />
+                                </div>
+                            </div>
+
+                            <div className="table__row">
+                                <div className="table__cell table__cell--left">
+                                    <strong className="table__cell-title">One-time adjustment charge</strong>
+
+                                    <p className="table__cell-text">Difference between new and current plan (charged immediately)</p>
+                                </div>
+
+                                <div className="table__cell table__cell--right">
+                                    <strong>{String.fromCharCode(36)}39.95</strong>
+                                </div>
+                            </div>
+
+                            <div className="table__row">
+                                <div className="table__cell table__cell--left">
+                                    <strong className="table__cell-title">Additional cost per month</strong>
+
+                                    <p className="table__cell-text">Your next payment (yearly) is on Nov. 26, 2018</p>
+                                </div>
+
+                                <div className="table__cell table__cell--right">
+                                    <strong>{String.fromCharCode(36)}100.00</strong>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="modal__footer">
+                            <div className="row align-items-center">
+                                <div className="col-md-8">
+                                    <p className="mb-0">1 seats will be added to your organization</p>
+                                </div>
+
+                                <div className="col-md-4">
+                                    <Button clickHandler={this.handleModal} label="Invite" type="primary" disabled={false} />
+                                </div>
+                            </div>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         )

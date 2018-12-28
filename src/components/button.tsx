dc9b09e43b7439658d72acb3f87db4a16f169757
Alex Devero <deveroalex@gmail.com>
2018-12-28 14:30:02 +0100

    Add full-btn variant

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,6 +5,7 @@ type ButtonProps = {
     clickHandler?: any;
     disabled?: boolean;
     hasSpinner?: boolean;
+    isFullWidth?: boolean;
     label: string;
     type: 'primary' | 'ghost';
 }
@@ -13,7 +14,7 @@ export class Button extends React.PureComponent<ButtonProps, {}> {
     // Render the app
     render() {
         return (<button
-            className={`btn ${this.props.type === 'primary' ? 'btn--primary' : 'btn--ghost'}`}
+            className={`btn ${this.props.type === 'primary' ? 'btn--primary' : 'btn--ghost'} ${this.props.isFullWidth ? 'btn--full' : ''}`}
             disabled={this.props.disabled ? true : false}
             onClick={this.props.clickHandler ? this.props.clickHandler : undefined}
         >

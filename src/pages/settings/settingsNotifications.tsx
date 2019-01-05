d12e44d5f7815738cb56ec89e920c984b799b00e
Alex Devero <deveroalex@gmail.com>
2019-01-05 09:40:08 +0100

    Improve responsive layout of notifications

--- src/pages/settings/settingsNotifications.tsx
+++ src/pages/settings/settingsNotifications.tsx
@@ -15,62 +15,62 @@ export class SettingsNotifications extends React.PureComponent<SettingsNotificat
 
                 <div className="table">
                     <div className="table__row">
-                        <div className="table__cell col-md-7">
+                        <div className="table__cell col-lg-7">
                             <p className="table__cell-title">Project added or created</p>
 
                             <p className="table__cell-text">Someone else creates or updates a project</p>
                         </div>
 
-                        <div className="table__cell col-md-2">
-                            <span className="table__cell-title">Email &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-2 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Email &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
 
-                        <div className="table__cell col-md-3">
-                            <span className="table__cell-title">Web notification &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-3 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Web notification &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
                     </div>
 
                     <div className="table__row">
-                        <div className="table__cell col-md-7">
+                        <div className="table__cell col-lg-7">
                             <p className="table__cell-title">Comment added</p>
 
                             <p className="table__cell-text">Someone else adds a comment</p>
                         </div>
 
-                        <div className="table__cell col-md-2">
-                            <span className="table__cell-title">Email &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-2 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Email &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
 
-                        <div className="table__cell col-md-3">
-                            <span className="table__cell-title">Web notification &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-3 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Web notification &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
                     </div>
 
                     <div className="table__row">
-                        <div className="table__cell col-md-7">
+                        <div className="table__cell col-lg-7">
                             <p className="table__cell-title">Comment resolved</p>
 
                             <p className="table__cell-text">Someone else resolves a comment</p>
                         </div>
 
-                        <div className="table__cell col-md-2">
-                            <span className="table__cell-title">Email &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-2 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Email &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
 
-                        <div className="table__cell col-md-3">
-                            <span className="table__cell-title">Web notification &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-3 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Web notification &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
                     </div>
 
                     <div className="table__row">
-                        <div className="table__cell col-md-6">
+                        <div className="table__cell col-lg-5">
                             <p className="table__cell-title">My actions</p>
 
                             <p className="table__cell-text">When you do any of the actions above</p>
                         </div>
 
-                        <div className="table__cell col-md-6">
-                            <span className="table__cell-title">Update me based on the notification preferences set above &nbsp;&nbsp;<Switch enabled={false} /></span>
+                        <div className="table__cell col-lg-7 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Update me based on the notification preferences set above &nbsp;&nbsp;<Switch enabled={false} /></span>
                         </div>
                     </div>
                 </div>
@@ -79,26 +79,26 @@ export class SettingsNotifications extends React.PureComponent<SettingsNotificat
 
                 <div className="table">
                     <div className="table__row">
-                        <div className="table__cell col-md-6">
+                        <div className="table__cell col-lg-6">
                             <p className="table__cell-title">Automatic subscription to personal projects</p>
 
                             <p className="table__cell-text">When a project is created in a personal workspace </p>
                         </div>
 
-                        <div className="table__cell col-md-6">
-                            <span className="table__cell-title">Automatically subscribe &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-6 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Automatically subscribe &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
                     </div>
 
                     <div className="table__row">
-                        <div className="table__cell col-md-6">
+                        <div className="table__cell col-lg-6">
                             <p className="table__cell-title">Automatic subscription to projects within organization</p>
 
                             <p className="table__cell-text">When a project is created within the organizations you are a member of </p>
                         </div>
 
-                        <div className="table__cell col-md-6">
-                            <span className="table__cell-title">Automatically subscribe &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-6 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Automatically subscribe &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
                     </div>
                 </div>
@@ -109,32 +109,32 @@ export class SettingsNotifications extends React.PureComponent<SettingsNotificat
 
                 <div className="table table--gray">
                     <div className="table__row">
-                        <div className="table__cell col-md-6">
+                        <div className="table__cell col-lg-6">
                             <p className="table__cell-title">Project 1</p>
                         </div>
 
-                        <div className="table__cell col-md-6">
-                            <span className="table__cell-title">Send notifications &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-6 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Send notifications &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
                     </div>
 
                     <div className="table__row">
-                        <div className="table__cell col-md-6">
+                        <div className="table__cell col-lg-6">
                             <p className="table__cell-title">Project 2</p>
                         </div>
 
-                        <div className="table__cell col-md-6">
-                            <span className="table__cell-title">Send notifications &nbsp;&nbsp;<Switch enabled={false} /></span>
+                        <div className="table__cell col-lg-6 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Send notifications &nbsp;&nbsp;<Switch enabled={false} /></span>
                         </div>
                     </div>
 
                     <div className="table__row">
-                        <div className="table__cell col-md-6">
+                        <div className="table__cell col-lg-6">
                             <p className="table__cell-title">Project 3</p>
                         </div>
 
-                        <div className="table__cell col-md-6">
-                            <span className="table__cell-title">Send notifications &nbsp;&nbsp;<Switch enabled={true} /></span>
+                        <div className="table__cell col-lg-6 align-items-start align-items-lg-end mt-lg-down-half">
+                            <span className="table__cell-title notification__label">Send notifications &nbsp;&nbsp;<Switch enabled={true} /></span>
                         </div>
                     </div>
                 </div>
